486d542332073cbc1402228c24b49b283cf548d7
ajkrenek <98238097+ajkrenek@users.noreply.github.com>
2022-09-10 11:31:09 -0500

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from django.core.management.utils import get_random_secret_key
 from pathlib import Path
 import os, sys, dj_database_url
 import environ
