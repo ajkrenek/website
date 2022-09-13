ce51ae49874852a68ac514fd3a45df7d474f4011
ajkrenek <98238097+ajkrenek@users.noreply.github.com>
2022-09-12 20:00:53 -0500

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     #text = models.TextField()
-    text = RichTextField(max_length=10000)
+    text = RichTextField(max_length=10000, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     slug = models.SlugField(max_length=200, unique=True)
