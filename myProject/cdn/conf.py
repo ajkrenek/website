132c32272653719ca63559018d8f8012d0a79690
ajkrenek <98238097+ajkrenek@users.noreply.github.com>
2022-09-13 20:50:14 -0500

    Update conf.py

--- myProject/cdn/conf.py
+++ myProject/cdn/conf.py
@@ -6,6 +6,7 @@ AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL="https://nyc3.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
+    "ACL": "public-read",
 }
 AWS_LOCATION = f"https://{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com"
 
